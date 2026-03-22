48141a2522ba6d1d3a288f5cf56e94006f4fe2fa
Srishti Rathi <110818209+SrishtiCode@users.noreply.github.com>
2026-03-22 13:58:56 +0530

    Update ForceSolution.s.sol

--- script/ForceSolution.s.sol
+++ script/ForceSolution.s.sol
@@ -20,38 +20,3 @@ contract ForceSolution is Script {
     }
 }
 
-/*
-Traces:
-  [44498] ForceSolution::run()
-    ├─ [0] VM::envUint("PRIVATE_KEY") [staticcall]
-    │   └─ ← [Return] <env var value>
-    ├─ [0] VM::startBroadcast(<pk>)
-    │   └─ ← [Return]
-    ├─ [7970] → new <unknown>@0xCd7d09c7F4c1cf545f43dB7403213eA488423BB3
-    │   └─ ← [Return] 0 bytes of code
-    ├─ [0] VM::stopBroadcast()
-    │   └─ ← [Return]
-    └─ ← [Stop]
-
-
-Script ran successfully.
-
-## Setting up 1 EVM.
-==========================
-Simulated On-chain Traces:
-
-  [7970] → new <unknown>@0xCd7d09c7F4c1cf545f43dB7403213eA488423BB3
-    └─ ← [Return] 0 bytes of code
-
-
-==========================
-
-Chain 11155111
-
-Estimated gas price: 0.00100002 gwei
-
-Estimated total gas used for script: 83584
-
-Estimated amount required: 0.00000008358567168 ETH
-
-========================== */
