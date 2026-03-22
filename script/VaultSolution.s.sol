9541670295db48b23971e199d9080648b0db5d0d
Srishti Rathi <110818209+SrishtiCode@users.noreply.github.com>
2026-03-22 13:54:20 +0530

    Update VaultSolution.s.sol

--- script/VaultSolution.s.sol
+++ script/VaultSolution.s.sol
@@ -17,30 +17,3 @@ contract VaultSolution is Script {
         vm.stopBroadcast();
     }
 }
-
-/*cast storage 0xa0fF6C04CFb4509064EF1b99F814c21645Eb552a 1 --rpc-url $RPC_URL
-0x412076657279207374726f6e67207365637265742070617373776f7264203a29
-
-Traces:
-  [16860] VaultSolution::run()
-    ├─ [0] VM::envUint("PRIVATE_KEY") [staticcall]
-    │   └─ ← [Return] <env var value>
-    ├─ [0] VM::startBroadcast(<pk>)
-    │   └─ ← [Return]
-    ├─ [7367] 0xa0fF6C04CFb4509064EF1b99F814c21645Eb552a::unlock(0x412076657279207374726f6e67207365637265742070617373776f7264203a29)
-    │   └─ ← [Stop]
-    ├─ [0] VM::stopBroadcast()
-    │   └─ ← [Return]
-    └─ ← [Stop]
-
-
-Script ran successfully.
-
-## Setting up 1 EVM.
-==========================
-Simulated On-chain Traces:
-
-  [7367] 0xa0fF6C04CFb4509064EF1b99F814c21645Eb552a::unlock(0x412076657279207374726f6e67207365637265742070617373776f7264203a29)
-    └─ ← [Stop]
-
- */
